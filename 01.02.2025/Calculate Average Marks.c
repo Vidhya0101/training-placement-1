68f0162d7769fd88e85105ef10b1fd7950dc9c06
Vidhya0101 <vidhya7117@gmail.com>
2025-02-02 18:29:44 +0530

    Calculate Average Marks.c

--- 01.02.2025/Calculate Average Marks.c
+++ 01.02.2025/Calculate Average Marks.c
@@ -7,6 +7,7 @@ if (mark1<0 || mark2<0 || mark3<0) {
   printf("Invalid input! Please try again with positive numbers..\n");
   return 1;
   }
+  
   float average = (mark1 + mark2 + mark3) /3.00;
   printf("Average Marks is: %.2f\n", average);
 return 0;
